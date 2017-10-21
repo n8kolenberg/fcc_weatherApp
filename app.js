a1c84c9c9b02f92e1eeac3942b38cbac1f214171
CRITEOIS\n.kolenberg <nkolenberg@gmail.com>
2017-10-21 11:08:37 +0400

    Made use of Openweather API. Added weatherIcons based on weathercode in Openweather API.

--- app.js
+++ app.js
@@ -18,49 +18,63 @@ var app = new Vue({
 		longitude: "",
 		temperature: "",
 		tempScale: "C",
+		humidity: "",
 		backgroundImage: "",
 		minTemp: "",
-		maxTemp: ""
+		maxTemp: "",
+		partOfDay: "day",
+		weatherCode: "",
+		weatherIcon: "",
 	},
 
 	mounted() {
 		//Request to get the user's position - This works faster than using the user's browser navigator object
 		axios.get("https://freegeoip.net/json/")
 		    .then(function(response) {
-		    	// console.log("location data: ");
-		    	// console.log(response);
+		    	console.log("location data: ");
+		    	console.log(response);
 		        this.app.country = response.data.country_name;
 		        this.app.city = response.data.city;
 		        this.app.latitude = response.data.latitude;
 		        this.app.longitude = response.data.longitude;
 		        this.app.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + response.data.city+"')";
-		        // console.log(this.app.backgroundImage);
 		        this.app.body.style.backgroundImage = this.app.backgroundImage;
 
 
-						//Request to get the weather data
-		        axios.get(`https://fcc-weather-api.glitch.me/api/current?lat=${this.app.latitude}&lon=${this.app.longitude}`)
+				//Request to get the weather data
+		        axios.get(`http://api.openweathermap.org/data/2.5/find?lat=${this.app.latitude}&lon=${this.app.longitude}&units=metric&APPID=70c6cab3644a8c0121ec7f04c30ffc9c`)
 		            .then(function(response) {
 		                // console.log("Weather data: ");
 		                // console.log(response);
 
-		                this.app.weather = titleCase(response.data.weather[0].description);
-		                this.app.temperature = response.data.main.temp;
-		                this.app.minTemp = response.data.main.temp_min;
-		                this.app.maxTemp = response.data.main.temp_max;
-		         
-		                
+		                this.app.weather = titleCase(response.data.list[0].weather[0].description);
+		                this.app.temperature = response.data.list[0].main.temp;
+		                this.app.minTemp = response.data.list[0].main.temp;
+		                this.app.maxTemp = response.data.list[0].main.temp;
+		                this.app.humidity = response.data.list[0].main.humidity;
+		                //I use the weatherCode to link to the weather icons
+		                this.app.weatherCode = response.data.list[0].weather[0].id;
+
+		                //Now we'll check the time of day to utilize the day/night weather icons
+						let date = new Date();
+						if(date > 19) {
+							this.partOfDay = "night";
+						}
+       
+				        //Weather icon CSS class build up to add to HTML
+				  		this.app.weatherIcon = `wIns wi wi-owm-${this.app.partOfDay}-${this.app.weatherCode}`;
 
 		            });
-
+        
 
 		    });
 
 	},
 
 	created() {
+		//This is to ensure the background gets the Unsplash image based on user's location
 		this.body.style.backgroundImage = this.backgroundImage;
-
+		
 	},
 
 	methods: {
